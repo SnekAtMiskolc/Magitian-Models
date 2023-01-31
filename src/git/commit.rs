e8e96cb20cd0ff3937b7b22fe03a394c4c92472c
Susa Milán <mailtosnek@protonmail.com>
2023-01-31 17:01:24 +0100

    Fixed Option<&str> issue with map

--- src/git/commit.rs
+++ src/git/commit.rs
@@ -5,16 +5,16 @@ use super::RawCommit;
 /// They are meant to be created from RawCommits which are extracted from incoming packfiles.
 /// They are both extracted on the http side and the ssh side.
 /// Only create them manually if the user uses the webapp to push to a repository.
-pub struct Commit<'a> {
+pub struct Commit {
     pub id: String,
-    pub name: Option<&'a str>,
+    pub name: Option<String>,
     pub email: String,
-    pub message: String,
+    pub message: Option<String>,
     pub commit: String,
 }
 
-impl<'a> Commit<'a> {
-    pub fn new(id: String, name: Option<&'a str>, email: String, message: String, commit: String) -> Self {
+impl Commit {
+    pub fn new(id: String, name: Option<String>, email: String, message: Option<String>, commit: String) -> Self {
         Self {
             id,
             name,
@@ -25,19 +25,15 @@ impl<'a> Commit<'a> {
     }
 }
 
-impl<'a> From<RawCommit> for Commit<'a> {
+impl From<RawCommit> for Commit {
     fn from(c: RawCommit) -> Self {
         // TODO: Change this to a Some(message) type deal
-        let message: String;
-        if let Some(m) = c.message() {
-            message = m.to_string();
-        } else {
-            message = String::from("")
-        }
+        let message = c.message().map(|m| m.to_string());
+        let name = c.committer().name().map(|n| n.to_string());
         Self {
             // Assign an UUID to the commit that will be stored in the database.
             id: uuid::Uuid::new_v4().to_string(),
-            name: c.committer().name(),
+            name,
             email: String::from("EMAIL"),
             // Extract the message from the commit.
             message,
