024232c3bb26f903ce6a8ba45e68442aaba711c0
Susa Milán <mailtosnek@protonmail.com>
2023-02-03 21:42:11 +0100

    Added Push

--- src/git/push.rs
+++ src/git/push.rs
@@ -1,3 +1,4 @@
+use serde_derive::{Deserialize, Serialize};
 
 /*
 Used for notifications.
@@ -5,8 +6,10 @@ If Someone pushes we:
  - write to the ODB
  - Extract the added commits
  - Assign commits that have been pushed in one request a pushID
-
 */ 
+#[derive(Serialize, Deserialize)]
 pub struct Push {
-
+    pub id: String,
+    // Optional since the user might not have an email address.
+    pub user_id: Option<String>,
 }
