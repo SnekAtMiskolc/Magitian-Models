9a9e4762b529c19db63617cc2c032e5c94a48725
Susa Milán <mailtosnek@protonmail.com>
2023-02-03 21:42:25 +0100

    pub mod some modules

--- src/git.rs
+++ src/git.rs
@@ -1,9 +1,18 @@
 use git2::{Commit, Repository};
 
+/// All commit related stuff.
 pub mod commit;
+/// Repository management.
 pub mod repository;
 
+// Other stuff.
+pub mod push;
+pub mod pr;
+pub mod merge;
+
 /// Wrap a ```git2::Commit<'static>``` as a ```RawCommit```.
+/// This is done so that we can distinguish between a ```Commit``` meant
+/// to be stored in the database and a ```RawCommit``` that is used for git operations.
 pub type RawCommit = Commit<'static>;
 
 /// Wrap a ```git2::Repository``` as a ```RawRepository```.
