0dc0094e62bbe7267e75fea359b3ff208f452719
Susa Milán <mailtosnek@protonmail.com>
2023-02-04 08:43:37 +0100

    New lifetime for RawCommit

--- src/git.rs
+++ src/git.rs
@@ -1,4 +1,4 @@
-use git2::{Commit, Repository};
+use git2::{Commit, Oid, Repository};
 
 /// All commit related stuff.
 pub mod commit;
@@ -6,14 +6,14 @@ pub mod commit;
 pub mod repository;
 
 // Other stuff.
-pub mod push;
-pub mod pr;
 pub mod merge;
+pub mod pr;
+pub mod push;
 
 /// Wrap a ```git2::Commit<'static>``` as a ```RawCommit```.
 /// This is done so that we can distinguish between a ```Commit``` meant
 /// to be stored in the database and a ```RawCommit``` that is used for git operations.
-pub type RawCommit = Commit<'static>;
+pub type RawCommit<'rawcommit> = Commit<'rawcommit>;
 
 /// Wrap a ```git2::Repository``` as a ```RawRepository```.
 pub type RawRepository = Repository;
