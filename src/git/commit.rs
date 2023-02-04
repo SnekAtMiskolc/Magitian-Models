8bb91695447337d7593b8292309279e81eb22fd7
Susa Milán <mailtosnek@protonmail.com>
2023-02-04 08:43:56 +0100

    Docs and the base of commit extraction

--- src/git/commit.rs
+++ src/git/commit.rs
@@ -1,9 +1,10 @@
 use super::RawCommit;
+use git2::{Oid, Repository, Revwalk};
 use serde_derive::{Deserialize, Serialize};
 
 /// # Commit
 /// The commit struct represents a commit in the database.
-/// They are meant to be created from ```RawCommits``` which are extracted from incoming packfiles.
+/// They are meant to be created from ```RawCommits``` which are extracted from the Object Database.
 /// They are both extracted on the http side and the ssh side.
 /// Only create them manually if the user uses the webapp to push to a repository.
 #[derive(Debug, Serialize, Deserialize)]
@@ -40,8 +41,22 @@ impl Commit {
     /// This means that we need to iterate objects in the repo's ODB (Object Database) and see if they
     /// are a commit, if that is the case then we check if it is already in the database if so
     /// then we shall not store it otherwise we push it to the ```Vec<Commit>``` after we use ```Into<RawCommit>``` on it.
-    pub fn from_unadded() -> Vec<Self> {
-        todo!()
+    pub fn from_unadded(repo: &Repository, marker: Option<&str>) -> Result<Vec<Self>, git2::Error> {
+        let mut walker = repo.revwalk()?;
+        if let Some(marker) = marker {
+            walker.push(Oid::from_str(marker)?)?
+        } else {
+            walker.push_head()?
+        }
+        let mut batch: Vec<RawCommit> = Vec::<RawCommit>::new();
+        const BATCH_SIZE: usize = 100;
+        for oid in walker {
+            let oid = oid?;
+            let commit = repo.find_commit(oid)?;
+            batch.push(commit);
+        }
+
+        Ok(batch.into_iter().map(|c| c.into()).collect())
     }
     pub fn insert() {
         todo!()
@@ -49,12 +64,18 @@ impl Commit {
     pub fn delete() {
         todo!()
     }
-    pub fn query_by_commit_id() {todo!()}
-    pub fn query_by_id() {todo!()}
-    pub fn query_by_user() {todo!()}
+    pub fn query_by_commit_id() {
+        todo!()
+    }
+    pub fn query_by_id() {
+        todo!()
+    }
+    pub fn query_by_user() {
+        todo!()
+    }
 }
 
-impl From<RawCommit> for Commit {
+impl From<RawCommit<'_>> for Commit {
     /// Create a new ```Commit``` from a ```RawCommit```.
     fn from(c: RawCommit) -> Self {
         Self {
