f10ccdc3ae9f9dea2ce825a9e73107b07e2cc67b
Susa Milán <mailtosnek@protonmail.com>
2023-02-04 08:44:09 +0100

    Cargo fmt

--- src/git/push.rs
+++ src/git/push.rs
@@ -2,11 +2,11 @@ use serde_derive::{Deserialize, Serialize};
 
 /*
 Used for notifications.
-If Someone pushes we: 
+If Someone pushes we:
  - write to the ODB
  - Extract the added commits
  - Assign commits that have been pushed in one request a pushID
-*/ 
+*/
 #[derive(Serialize, Deserialize)]
 pub struct Push {
     pub id: String,
